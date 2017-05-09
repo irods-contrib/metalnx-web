33e2ea09404cb7fd424e8a1398eb3f280f36a36f
guerra <arthur.guerra@emc.com>
2017-05-09 14:58:07 -0300

    Added attributes to ticket

--- src/emc-metalnx-core/src/main/java/com/emc/metalnx/core/domain/entity/DataGridTicket.java
+++ src/emc-metalnx-core/src/main/java/com/emc/metalnx/core/domain/entity/DataGridTicket.java
@@ -20,4 +20,66 @@ package com.emc.metalnx.core.domain.entity;
  * Class that represents a ticket.
  */
 public class DataGridTicket {
+    private String ticketString, path, owner;
+    private TicketType type;
+    private boolean isCollection;
+
+    public enum TicketType {
+        READ, WRITE, UNKNOWN;
+    }
+
+    public void setTicketString(String ticketString) {
+        this.ticketString = ticketString;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setType(TicketType type) {
+        this.type = type;
+    }
+
+    /**
+     * Tells whether or not the ticket is for a collection.
+     * @param isTicketForCollection True if the path associated to the ticket is a collection. False, otherwise.
+     */
+    public void setIsCollection(boolean isTicketForCollection) {
+        isCollection = isTicketForCollection;
+    }
+
+    public String getTicketString() {
+        return ticketString;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public TicketType getType() {
+        return type;
+    }
+
+    public boolean isCollection() {
+        return isCollection;
+    }
+
+    @Override
+    public String toString() {
+        return "DataGridTicket{" +
+                "ticketString='" + ticketString + '\'' +
+                ", path='" + path + '\'' +
+                ", owner='" + owner + '\'' +
+                ", type=" + type +
+                ", isCollection=" + isCollection +
+                '}';
+    }
 }
