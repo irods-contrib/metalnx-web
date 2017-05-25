c8cf5bb8809ac7d639ed91a0203a9dc5058c8e6b
guerra <arthur.guerra@emc.com>
2017-05-25 12:09:58 -0300

    Implemented a Date deserializer to convert JSON dates properly into the ticket

---
+++ src/emc-metalnx-core/src/main/java/com/emc/metalnx/core/domain/utils/DataGridJsonDateDeserializer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015-2017, Dell EMC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.emc.metalnx.core.domain.utils;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Custom deserializer for dates as a JSON.
+ */
+public class DataGridJsonDateDeserializer extends JsonDeserializer<Date> {
+    @Override
+    public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String date = jsonParser.getText();
+
+        try {
+            return format.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
