6392f5e513690fd80fd7f80a761eef1663dfb48e
guerra <arthur.guerra@emc.com>
2017-05-30 14:50:59 -0300

    Removed file uploaded during the test

--- src/emc-metalnx-services/src/test/java/com/emc/metalnx/services/tests/ticketclient/TestTicketWithByteLimit.java
+++ src/emc-metalnx-services/src/test/java/com/emc/metalnx/services/tests/ticketclient/TestTicketWithByteLimit.java
@@ -16,6 +16,7 @@
 
 package com.emc.metalnx.services.tests.ticketclient;
 
+import com.emc.metalnx.core.domain.exceptions.DataGridConnectionRefusedException;
 import com.emc.metalnx.core.domain.exceptions.DataGridException;
 import com.emc.metalnx.core.domain.exceptions.DataGridTicketUploadException;
 import com.emc.metalnx.services.interfaces.IRODSServices;
@@ -58,7 +59,7 @@ public class TestTicketWithByteLimit {
     @Autowired
     private IRODSServices irodsServices;
 
-    private String ticketString, targetPath;
+    private String ticketString, targetPath, filePath;
     private TestTicketUtils ticketUtils;
     private File localFile;
 
@@ -70,12 +71,14 @@ public class TestTicketWithByteLimit {
         ticketString = ticketUtils.createTicket(parentPath, username, TicketCreateModeEnum.WRITE);
         ticketUtils.setWriteByteLimit(ticketString, WRITE_BYTE_LIMIT);
         localFile = ticketUtils.createLocalFile();
+        filePath = String.format("%s/%s", targetPath, localFile.getName());
     }
 
     @After
-    public void tearDown() throws JargonException {
+    public void tearDown() throws JargonException, DataGridConnectionRefusedException {
         FileUtils.deleteQuietly(localFile);
         ticketUtils.deleteTicket(ticketString);
+        ticketUtils.deleteIRODSFile(filePath);
     }
 
     @Test(expected = DataGridTicketUploadException.class)
