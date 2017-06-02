6f9b7d4c3d4f87702c2254c797c604246d249944
Mileine Assato <mileine.assato@emc.com>
2017-06-02 13:17:06 -0300

    Edit hostname on ticket form.

--- src/emc-metalnx-shared/src/main/resources/static/js/ticketForm.js
+++ src/emc-metalnx-shared/src/main/resources/static/js/ticketForm.js
@@ -28,18 +28,38 @@ function serializeTicketForm(){
 
     var form = $("#ticketFormPanel form").serializeArray();
     var ticket = {};
+    ticket['hosts'] = [];
     for (var i = 0; i < form.length; i++) {
         var name = form[i]['name'];
         var values = form[i]['value'].split(',');
 
-        if (name == 'users' || name == 'groups' || name == 'hosts') {
+        if (name == 'users' || name == 'groups') {
             ticket[name] = [];
             for (var v = 0; v < values.length; v++) {
                 ticket[name].push(values[v]);
             }
-        } else {
+        }
+        else if(name == 'hosts'){
+            ticket[name].push(values[0]);
+        }
+        else {
             ticket[name] = values[0];
         }
     }
     return ticket;
+}
+
+function removeHost(hostname){
+    var li_id = 'li_' + hostname;
+    var inputHost_id = 'input_' +  hostname;
+
+
+    var li = document.getElementById(li_id);
+    var inputHost =  document.getElementById(inputHost_id);
+    var ul = li.parentElement;
+    var panelHosts = inputHost.parentElement;
+
+    ul.removeChild(li);
+    ul.removeChild(inputHost);
+
 }
