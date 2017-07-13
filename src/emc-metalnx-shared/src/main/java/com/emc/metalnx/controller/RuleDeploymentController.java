b3387f8dfeb6fa3e8479338e32dd9d0ecbf57687
guerra <arthur.guerra@emc.com>
2017-07-13 18:15:17 -0300

    Removed unnecessary methods

--- src/emc-metalnx-shared/src/main/java/com/emc/metalnx/controller/RuleDeploymentController.java
+++ src/emc-metalnx-shared/src/main/java/com/emc/metalnx/controller/RuleDeploymentController.java
@@ -16,19 +16,15 @@
 
 package com.emc.metalnx.controller;
 
-import com.emc.metalnx.core.domain.exceptions.DataGridException;
 import com.emc.metalnx.services.interfaces.RuleDeploymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @Scope(WebApplicationContext.SCOPE_SESSION)
@@ -44,23 +40,5 @@ public class RuleDeploymentController {
     public String index() {
         logger.info("Rules page");
         return "rules/rulesManagement";
-    }
-
-    @RequestMapping(value = "/findall", method = RequestMethod.GET)
-    public String findAll() {
-        logger.info("Find all rules");
-        return "rules/rulesList";
-    }
-
-    @RequestMapping(value = "/upload", method = RequestMethod.GET)
-    public ResponseEntity<?> upload()
-            throws DataGridException {
-        logger.info("Uploading rule ...");
-
-        //MultipartFile multipartFile = multipartRequest.getFile("file");
-        MultipartFile multipartFile =  new MockMultipartFile("TEST_RULE_NAME.re", "Hello World".getBytes());
-
-        ruleDeploymentService.deployRule(multipartFile);
-        return null;
     }
 }
