cac02dc3326fd6a6821144d6e8d59495ee5510d9
michael-conway <michael.c.conway@gmail.com>
2017-09-27 17:21:03 -0400

    #2 build now works in docker

--- packaging/docker/start_metalnx.sh
+++ packaging/docker/start_metalnx.sh
@@ -73,8 +73,8 @@ __setup_certs() {
 	if [ -f /tmp/cert/server.crt ];
 		then
 	   		echo "Cert will be imported"
-	   		keytool -delete -noprompt -alias mycert -keystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -storepass changeit
-	   		keytool -import -trustcacerts -keystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -storepass changeit -noprompt -alias mycert -file /tmp/cert/server.crt
+	   		keytool -delete -noprompt -alias mycert -keystore /usr/lib/jvm/jre/lib/security/cacerts -storepass changeit
+	   		keytool -import -trustcacerts -keystore /usr/lib/jvm/jre/lib/security/cacerts -storepass changeit -noprompt -alias mycert -file /tmp/cert/server.crt
 		else
 	   		echo "No cert to import"
 	fi
