9796c4bdc864573a65fc8374272ad9a7636d6fee
Hetal Patel <hetalben.patel@nih.gov>
2018-01-29 10:23:20 -0500

    #27 sop in controller

--- src/emc-metalnx-shared/src/main/java/com/emc/metalnx/controller/CollectionInfoController.java
+++ src/emc-metalnx-shared/src/main/java/com/emc/metalnx/controller/CollectionInfoController.java
@@ -33,6 +33,7 @@ import org.springframework.web.servlet.HandlerMapping;
 import com.emc.metalnx.controller.utils.LoggedUserUtils;
 import com.emc.metalnx.core.domain.exceptions.DataGridConnectionRefusedException;
 import com.emc.metalnx.core.domain.exceptions.DataGridException;
+import com.emc.metalnx.modelattribute.breadcrumb.DataGridBreadcrumb;
 import com.emc.metalnx.services.interfaces.CollectionService;
 import com.emc.metalnx.services.interfaces.IRODSServices;
 import com.emc.metalnx.services.interfaces.IconService;
@@ -85,6 +86,9 @@ public class CollectionInfoController {
 		
 		model.addAttribute("iconToDisplay", iconToDisplay);				
 		model.addAttribute("dataProfile", dataProfile);
+		model.addAttribute("breadcrumb", new DataGridBreadcrumb(dataProfile.getAbsolutePath()));
+		
+		System.out.println("Absolute Path :: " +dataProfile.getAbsolutePath());
 		String template = "";
 		
 		if(!dataProfile.isFile()) 
