7f2364642be571fd99523e8df717b5b004c00930
Hetal Patel <hetalben.patel@nih.gov>
2018-02-09 14:25:49 -0500

    #53 tabs loader fix

--- src/emc-metalnx-shared/src/main/resources/static/js/collection.js
+++ src/emc-metalnx-shared/src/main/resources/static/js/collection.js
@@ -8,22 +8,15 @@ function getCollectionSummary(path){
 function displayCollectionSummary(data){
 	console.log("displayTestDetails()");	
 	$("#summary").html(data);	
-	//displayInfoDetails(data);	
 }
-
 
 function getInfoDetails(path){
-	//$("#info").hide();
-	//$("#table-loader").show();	
 	window.location.hash = "info";	
 	var url = "/emc-metalnx-web/collectionInfo/collectionFileInfo/";
-	ajaxEncapsulation(url, "POST", {path: path}, displayInfoDetails, null, null, null);
-	
+	ajaxEncapsulation(url, "POST", {path: path}, displayInfoDetails, null, null, null);	
 }
 
-function getMetadata(path){
-	$("#metadata").hide();
-	$("#table-loader").show();		
+function getMetadata(path){	
 	console.log("Collection getMetadata() :: " +path);
 	window.location.hash = "metadata";
 	var url = "/emc-metalnx-web/metadata/getMetadata/";	
@@ -31,8 +24,6 @@ function getMetadata(path){
 }
 
 function getPermissionDetails(path){
-	$("#permission").hide();
-	$("#table-loader").show();		
 	console.log("Collection getPermDetails() :: " +path);
 	window.location.hash = "permission";
 	var url = "/emc-metalnx-web/permissions/getPermissionDetails/";	
@@ -46,62 +37,20 @@ function getPerview(path){
 	ajaxEncapsulation(url, "GET", {path:path}, displayPreviewImage, null, null);	
 }
 
-function getPermDetails(path){
-    $('#table-loader').nextAll().remove();
-	$('#table-loader').show();
-	$("#uploadIcon").prop("disabled", true);
-    $("#uploadIcon").addClass("disabled");
-    $("#showCollectionFormBtn").prop("disabled", true);
-    $("#showCollectionFormBtn").addClass("disabled");
-	var url = "/emc-metalnx-web/permissions/getPermissionDetails/";
-
-	ajaxEncapsulation(url, "POST", {path: path}, displayPermDetails, null, null);
-}
-
-function displayPermDetails(data){
-    $('#table-loader').hide();
-    $("#table-loader").after(data);
-    $("#uploadIcon").prop("disabled", true);
-    $("#uploadIcon").addClass("disabled");
-    $("#showCollectionFormBtn").prop("disabled", true);
-    $("#showCollectionFormBtn").addClass("disabled");
-}
-
-function displayPreviewImage(data){
-	//$("#previewFile").attr("src","data:image/png;base64," + data);	
-	$("#preview").html(data);
-	
-}
-
-function displayInfoDetails(data){
-	//$("#table-loader").hide();
+function displayInfoDetails(data){	
 	$("#info").html(data);
-	//$("#info").show();
 }
 
-function displayMetadata(data){
-	console.log("display Metadata :: " +data);
-	$('#table-loader').hide();
-	$('#table-loader').after(data);
-	/*$("#uploadIcon").prop("disabled", true);
-    $("#uploadIcon").addClass("disabled");
-    $("#showCollectionFormBtn").prop("disabled", true);
-    $("#showCollectionFormBtn").addClass("disabled");*/
+function displayMetadata(data){	
 	$("#metadata").html(data);	
-	//$("#metadata").show();
 }
 
 function displayPermissionDetails(data){
-	console.log("display Permission");
-	//$("#table-loader").hide();
-	 $('#table-loader').hide();
-     $("#table-loader").after(data);
-     /*$("#uploadIcon").prop("disabled", true);
-     $("#uploadIcon").addClass("disabled");
-     $("#showCollectionFormBtn").prop("disabled", true);
-     $("#showCollectionFormBtn").addClass("disabled");*/
 	$('#permission').html(data);
-	$("#permission").show();    
+}
+
+function displayPreviewImage(data){
+	$("#preview").html(data);	
 }
 
 function showPreview(){
@@ -167,13 +116,11 @@ function positionBrowserToPath(path) {
 }
 
 function fileDownload(path){
-	console.log("File Download");
-	
+	console.log("File Download");	
 	var prepareDownloadURL = "/emc-metalnx-web/fileOperation/prepareFilesForDownload/";
 	var paths = [];
 	paths.push(path);
 	ajaxEncapsulation(prepareDownloadURL, "GET", {paths: paths}, handleDownload, null);
-	//$("#actions button").prop("disabled", true);
 }
 
 function deleteInfoAction(path){
@@ -205,23 +152,4 @@ function handleDownload() {
 	window.location.href = "/emc-metalnx-web/fileOperation/download/";
 }
 
-/*function handleDownloadFailure() {
-	console.log(handleDownloadFailure);
-}
-*/
-/*
-
-function ChangeUrl(title, urlVal) {
-	console.log("ChangeUrl()");
-	var str1 = "emc-metalnx-web/collections";	
-	var url = str1.concat(urlVal);	
-	console.log("url :: " +url);	
-
-    if (typeof (history.pushState) != "undefined") {
-        var obj = { Title: title, Url: url };
-        history.pushState(obj, obj.Title, obj.Url);
-    } else {
-        alert("Browser does not support HTML5.");
-    }
-}*/
 
