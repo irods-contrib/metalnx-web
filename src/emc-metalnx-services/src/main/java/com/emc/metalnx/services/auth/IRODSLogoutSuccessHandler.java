e73b46239ca04e587a0bbc542b3b3ed6fd6efd9e
Mike Conway <mike.conway@nih.gov>
2018-03-23 14:01:43 -0400

    #164

--- src/emc-metalnx-services/src/main/java/com/emc/metalnx/services/auth/IRODSLogoutSuccessHandler.java
+++ src/emc-metalnx-services/src/main/java/com/emc/metalnx/services/auth/IRODSLogoutSuccessHandler.java
@@ -16,6 +16,13 @@
 
 package com.emc.metalnx.services.auth;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.io.FileUtils;
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
@@ -26,52 +33,47 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-
 public class IRODSLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler implements LogoutSuccessHandler {
 
-    @Autowired
-    IRODSAccessObjectFactory irodsAccessObjectFactory;
+	@Autowired
+	IRODSAccessObjectFactory irodsAccessObjectFactory;
 
-    private static final Logger logger = LoggerFactory.getLogger(IRODSLogoutSuccessHandler.class);
+	private static final Logger logger = LoggerFactory.getLogger(IRODSLogoutSuccessHandler.class);
 
-    @Override
-    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-            throws IOException, ServletException {
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
 
-        logger.info("Logging out...");
+		logger.info("Logging out...");
 
-        try {
-            IRODSAccount irodsAccount = ((UserTokenDetails) authentication.getDetails()).getIrodsAccount();
-            String username = irodsAccount.getUserName();
+		try {
+			IRODSAccount irodsAccount = ((UserTokenDetails) authentication.getDetails()).getIrodsAccount();
+			String username = irodsAccount.getUserName();
 
-            logger.info("Closing session and eating all exceptions");
-            irodsAccessObjectFactory.closeSessionAndEatExceptions(irodsAccount);
-            irodsAccessObjectFactory.closeSessionAndEatExceptions();
+			logger.info("Closing session and eating all exceptions");
+			irodsAccessObjectFactory.closeSessionAndEatExceptions(irodsAccount);
+			irodsAccessObjectFactory.closeSessionAndEatExceptions();
 
-            logger.debug("Removing current session temporary directory for file upload");
-            try {
-                File tmpSessionDir = new File(username);
-                if (tmpSessionDir.exists()) {
-                    FileUtils.forceDelete(tmpSessionDir);
-                }
-            }
-            catch (Exception e) {
-                logger.error("User {} temporary directory for upload does not exist.", username);
-            }
+			logger.debug("Removing current session temporary directory for file upload");
+			try {
+				File tmpSessionDir = new File(username);
+				if (tmpSessionDir.exists()) {
+					FileUtils.forceDelete(tmpSessionDir);
+				}
+			} catch (Exception e) {
+				logger.error("User {} temporary directory for upload does not exist.", username);
+			}
 
-            response.sendRedirect("/emc-metalnx-web/login/");
-            logger.info("User {} disconnected successfully", username);
-        }
-        catch (Exception e) {
-            logger.info("User session is already expired. There is no need to clear session.");
-        }
+			logger.info("invalidating session");
+			request.getSession().invalidate();
 
-        super.onLogoutSuccess(request, response, authentication);
-    }
+			response.sendRedirect("/emc-metalnx-web/login/");
+			logger.info("User {} disconnected successfully", username);
+		} catch (Exception e) {
+			logger.info("User session is already expired. There is no need to clear session.");
+		}
+
+		super.onLogoutSuccess(request, response, authentication);
+	}
 
 }
