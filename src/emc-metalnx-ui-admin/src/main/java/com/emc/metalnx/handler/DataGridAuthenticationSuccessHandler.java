bbbdaeee98fe04cf8f79c6e85948e1c43d5454a7
Mike Conway <mike.conway@nih.gov>
2018-07-11 15:12:30 -0400

    #141 additional fix to remove session invalidation handler, seems to resolve the noted scenario

--- src/emc-metalnx-ui-admin/src/main/java/com/emc/metalnx/handler/DataGridAuthenticationSuccessHandler.java
+++ src/emc-metalnx-ui-admin/src/main/java/com/emc/metalnx/handler/DataGridAuthenticationSuccessHandler.java
@@ -16,32 +16,35 @@
 
 package com.emc.metalnx.handler;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 public class DataGridAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
 	private static final Logger logger = LoggerFactory.getLogger(DataGridAuthenticationSuccessHandler.class);
-	/*
-	 * @Override public void onAuthenticationSuccess(HttpServletRequest request,
-	 * HttpServletResponse response, Authentication authentication) throws
-	 * IOException, ServletException {
-	 * 
-	 * logger.debug("Authentication successful!");
-	 * 
-	 * String username = (String) authentication.getPrincipal();
-	 * logger.debug("Applying user preferences to user [" + username + "]");
-	 * 
-	 * DataGridUser loggedUser =
-	 * userService.findByUsernameAndAdditionalInfo(username, zoneName);
-	 * localeResolver.setLocale(request, response,
-	 * StringUtils.parseLocaleString(loggedUser.getLocale()));
-	 * if(loggedUser.isAdmin()){
-	 * response.sendRedirect("/emc-metalnx-web/dashboard/"); }else{
-	 * response.sendRedirect("/emc-metalnx-web/browse/home"); }
-	 * 
-	 * }
-	 */
+	private RequestCache requestCache = new HttpSessionRequestCache();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws ServletException, IOException {
+		logger.info("onAuthenticationSuccess()...delegating to super");
+
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+		logger.debug("savedRequest:{}", savedRequest);
+
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
 
 }
