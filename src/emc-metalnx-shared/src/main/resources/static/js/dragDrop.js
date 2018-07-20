5aeccb2576957d73cdf7e0f4e36d2c01b07a165b
Kyle <kyle.rogers533@gmail.com>
2018-07-20 12:56:21 -0500

    Adding code for drag and drop file uploading

--- src/emc-metalnx-shared/src/main/resources/static/js/dragDrop.js
+++ src/emc-metalnx-shared/src/main/resources/static/js/dragDrop.js
@@ -1,1 +1,27 @@
 /* This is code that will allow the dragging and dropping of files to upload */
+
+var isAdvancedUpload = function() {
+   var div = document.createElement('div');
+   return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+}();
+
+var $form = $('#uploadModal');
+
+if (isAdvancedUpload) {
+  $form.addClass('has-advanced-upload');
+
+	var droppedFiles = false;
+	$form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+	  e.preventDefault();
+    e.stopPropagation();
+	})
+	.on('dragover dragenter', function() {
+    $form.addClass('is-dragover');
+	})
+	.on('dragleave dragend drop', function() {
+		$form.removeClass('is-dragover');
+	})
+	.on('drop', function(e) {
+		droppedFiles = e.originalEvent.dataTransfer.files;
+	});
+}
