9a261abcf325383d56f5c2d807e56109c2114fd0
Mike Conway <mike.conway@nih.gov>
2018-09-20 14:18:27 -0400

    #72 copy of coll abs path

--- src/emc-metalnx-shared/src/main/resources/static/js/copyToClipboard.js
+++ src/emc-metalnx-shared/src/main/resources/static/js/copyToClipboard.js
@@ -1,7 +1,7 @@
  /* Copyright (c) 2018, University of North Carolina at Chapel Hill */
  /* Copyright (c) 2015-2017, Dell EMC */
  
-
+// TODO: replace with clipboard.js
 function setCopyTicketBtn(){
     var copyTicketBtn = document.querySelector('.copyTicketBtn');
     copyTicketBtn.addEventListener('click', function(event) {
