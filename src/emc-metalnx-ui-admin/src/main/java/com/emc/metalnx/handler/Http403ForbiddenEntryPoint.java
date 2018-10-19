bad3332ecdf383ecbd7440a2613fe8fcf3595554
Mike Conway <mike.conway@nih.gov>
2018-10-19 11:35:52 -0400

    #204 add filter class and test logging

--- src/emc-metalnx-ui-admin/src/main/java/com/emc/metalnx/handler/Http403ForbiddenEntryPoint.java
+++ src/emc-metalnx-ui-admin/src/main/java/com/emc/metalnx/handler/Http403ForbiddenEntryPoint.java
@@ -11,17 +11,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-
 public class Http403ForbiddenEntryPoint implements AuthenticationEntryPoint {
 	private static final Logger logger = LoggerFactory.getLogger(Http403ForbiddenEntryPoint.class);
 
 	@Override
-	public void commence(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException arg2) throws IOException, ServletException {
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
+			throws IOException, ServletException {
 		logger.info("Http403ForbiddenEntryPoint : commence");
-		if (logger.isDebugEnabled()) {
-			logger.debug("Pre-authenticated entry point called. Rejecting access");
-		}
+		logger.debug("Pre-authenticated entry point called. Rejecting access");
+
 		response.sendRedirect("/emc-metalnx-web/login/exception/");
 	}
 }
