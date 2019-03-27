c046f7540baa7d5553fe5d3da08a681cd818aa53
Mike Conway <mike.conway@nih.gov>
2019-03-27 09:35:12 -0400

    #98 unit test captures bug

--- src/emc-metalnx-services/src/test/java/com/emc/metalnx/services/tests/metadata/TestMetadataCase.java
+++ src/emc-metalnx-services/src/test/java/com/emc/metalnx/services/tests/metadata/TestMetadataCase.java
@@ -9,9 +9,25 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import org.irods.jargon.core.connection.IRODSAccount;
+import org.irods.jargon.core.pub.CollectionAO;
+import org.irods.jargon.core.pub.DataObjectAO;
+import org.irods.jargon.core.pub.DataTransferOperations;
+import org.irods.jargon.core.pub.IRODSFileSystem;
+import org.irods.jargon.core.pub.domain.AvuData;
+import org.irods.jargon.core.pub.io.IRODSFile;
+import org.irods.jargon.testutils.AssertionHelper;
+import org.irods.jargon.testutils.IRODSTestSetupUtilities;
+import org.irods.jargon.testutils.TestingPropertiesHelper;
+import org.irods.jargon.testutils.filemanip.FileGenerator;
+import org.irods.jargon.testutils.filemanip.ScratchFileUtils;
 import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,6 +82,37 @@ public class TestMetadataCase {
 
 	private List<DataGridMetadataSearch> search;
 
+	private static Properties testingProperties = new Properties();
+	private static TestingPropertiesHelper testingPropertiesHelper = new TestingPropertiesHelper();
+	private static ScratchFileUtils scratchFileUtils = null;
+	public static final String IRODS_TEST_SUBDIR_PATH = "TestMetadataCase";
+	private static IRODSTestSetupUtilities irodsTestSetupUtilities = null;
+	private static AssertionHelper assertionHelper = null;
+	private static IRODSFileSystem irodsFileSystem;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		TestingPropertiesHelper testingPropertiesLoader = new TestingPropertiesHelper();
+		testingProperties = testingPropertiesLoader.getTestProperties();
+		scratchFileUtils = new ScratchFileUtils(testingProperties);
+		scratchFileUtils.clearAndReinitializeScratchDirectory(IRODS_TEST_SUBDIR_PATH);
+		irodsTestSetupUtilities = new IRODSTestSetupUtilities();
+		irodsTestSetupUtilities.initializeIrodsScratchDirectory();
+		irodsTestSetupUtilities.initializeDirectoryForTest(IRODS_TEST_SUBDIR_PATH);
+		assertionHelper = new AssertionHelper();
+		irodsFileSystem = IRODSFileSystem.instance();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		irodsFileSystem.closeAndEatExceptions();
+	}
+
+	@After
+	public void afterEach() throws Exception {
+		irodsFileSystem.closeAndEatExceptions();
+	}
+
 	@Before
 	public void setUp() throws DataGridException {
 		parentPath = String.format("/%s/home/%s", zone, username);
@@ -108,6 +155,66 @@ public class TestMetadataCase {
 	public void testCaseInsensitiveMetadataSearchContains() throws DataGridException {
 		search.add(new DataGridMetadataSearch(attr, val, unit, DataGridSearchOperatorEnum.LIKE));
 		assertMetadataSearch(3, 1);
+	}
+
+	/**
+	 * For https://github.com/irods-contrib/metalnx-web/issues/98
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSearchFilesAndCollectionsBug98() throws Exception {
+		String testSubdir = "testSearchFilesAndCollectionsBug98";
+		String testFileName = "testSearchFilesAndCollectionsBug98.dat";
+		String absPath = scratchFileUtils.createAndReturnAbsoluteScratchPath(IRODS_TEST_SUBDIR_PATH);
+		String localFileName = FileGenerator.generateFileOfFixedLengthGivenName(absPath, testFileName, 10);
+
+		IRODSAccount irodsAccount = testingPropertiesHelper.buildIRODSAccountFromTestProperties(testingProperties);
+
+		// put scratch file into irods in the right place on the first resource
+
+		String targetIrodsCollection = testingPropertiesHelper.buildIRODSCollectionAbsolutePathFromTestProperties(
+				testingProperties, IRODS_TEST_SUBDIR_PATH + "/" + testSubdir);
+
+		IRODSFile testDir = irodsFileSystem.getIRODSFileFactory(irodsAccount).instanceIRODSFile(targetIrodsCollection);
+		testDir.mkdirs();
+
+		String dataObjectAbsPath = targetIrodsCollection + '/' + testFileName;
+
+		DataTransferOperations dto = irodsFileSystem.getIRODSAccessObjectFactory()
+				.getDataTransferOperations(irodsAccount);
+		dto.putOperation(localFileName, targetIrodsCollection, irodsAccount.getDefaultStorageResource(), null, null);
+
+		DataObjectAO dataObjectAO = irodsFileSystem.getIRODSAccessObjectFactory().getDataObjectAO(irodsAccount);
+		CollectionAO collectionAO = irodsFileSystem.getIRODSAccessObjectFactory().getCollectionAO(irodsAccount);
+
+		// initialize the AVU data
+		String expectedAttribName = "testfbmdattrib1bug98";
+		String expectedAttribValue = "testfbmdvalue1bug98";
+		String expectedAttribUnits = "test1fbmdunitsbug98";
+
+		AvuData avuData = AvuData.instance(expectedAttribName, expectedAttribValue, expectedAttribUnits);
+
+		dataObjectAO.deleteAVUMetadata(dataObjectAbsPath, avuData);
+		dataObjectAO.addAVUMetadata(dataObjectAbsPath, avuData);
+		collectionAO.deleteAllAVUMetadata(targetIrodsCollection);
+		collectionAO.addAVUMetadata(targetIrodsCollection, avuData);
+
+		// now do the query and expect the data object and collection
+
+		List<DataGridMetadataSearch> testSearch = new ArrayList<>();
+		testSearch.add(new DataGridMetadataSearch(expectedAttribName, expectedAttribValue, expectedAttribUnits,
+				DataGridSearchOperatorEnum.EQUAL));
+		objs = metadataService.findByMetadata(testSearch, new DataGridPageContext(), 1, 100);
+		int ctr = 0;
+		for (DataGridCollectionAndDataObject obj : objs) {
+			if (!obj.getPath().contains("trash")) {
+				ctr++;
+			}
+		}
+
+		Assert.assertEquals(2, ctr);
+
 	}
 
 	private void assertMetadataSearch(int expectedNumOfFiles, int expectedNumOfMatchesByFile) throws DataGridException {
