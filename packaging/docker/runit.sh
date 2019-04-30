dca868dd88364fc0455b683ef9761a14f0e63b8d
Terrell Russell <terrellrussell@gmail.com>
2019-04-30 08:00:29 -0400

    [#104] skip failure to remove non-existent keystore alias

--- packaging/docker/runit.sh
+++ packaging/docker/runit.sh
@@ -3,7 +3,9 @@
 if [ -f /tmp/cert/server.crt ];
 then
    echo "Cert will be imported"
+   set +e
    keytool -delete -noprompt -alias mycert -keystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -storepass changeit
+   set -e
    keytool -import -trustcacerts -keystore /usr/lib/jvm/default-jvm/jre/lib/security/cacerts -storepass changeit -noprompt -alias mycert -file /tmp/cert/server.crt
 else
    echo "No cert to import"
