854d45346cf9f497d3ad476897ca03f9b2556492
Mike Conway <mike.conway@nih.gov>
2019-06-11 16:58:24 -0400

    #63 first vue.js page for collections

--- src/metalnx-web/vue.config.js
+++ src/metalnx-web/vue.config.js
@@ -1,10 +1,17 @@
 // vue.config.js
 module.exports = {
-  outputDir: 'target/dist',
+  outputDir: 'target/dist/static',
   filenameHashing: false,
+  publicPath: '/metalnx/static/',
   pages: {
    collections: {
      entry: 'src/main/javascript/collections/main.js'
+   },
+   home: {
+     entry: 'src/main/javascript/home/main.js'
+   },
+   notifications: {
+     entry: 'src/main/javascript/notifications/main.js'
    }
  }
 }
