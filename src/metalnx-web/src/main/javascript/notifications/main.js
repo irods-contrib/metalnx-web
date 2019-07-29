6f600206a8162d2cace541d4d44670d5ebf43ad8
deep.patel <deep.patel@nih.gov>
2019-07-29 08:21:23 -0400

    #110 vue integration for notifications

--- src/metalnx-web/src/main/javascript/notifications/main.js
+++ src/metalnx-web/src/main/javascript/notifications/main.js
@@ -1,20 +1,13 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
-import axios from 'axios'
-
-Vue.use(BootstrapVue)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import router from './router'
 
 Vue.config.productionTip = false
 
-// refactor to a properties
-
-//axios.defaults.baseURL = "http://localhost:8888/v2"
-//axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
