5306dd16567e8a9ec45d962702d3837f1d1c163b
Mike Conway <mike.conway@nih.gov>
2019-08-28 14:45:03 -0400

    #110 search schema select is live

--- src/metalnx-web/src/main/javascript/search/components/Search.vue
+++ src/metalnx-web/src/main/javascript/search/components/Search.vue
@@ -2,15 +2,15 @@
   <div class="container">
     <div class="row">
       <b-input-group>
-      <b-dropdown id="schemaSelection" slot="prepend" text="Dropdown" variant="info">
-              <b-dropdown-item  v-for="i in availableSearchSchema" :key="i" >{{i.schemaName}}</b-dropdown-item>
+      <b-dropdown id="schemaSelection" slot="prepend" v-bind:text="searchLabel" variant="info">
+              <b-dropdown-item  v-for="i in availableSearchSchema" :key="i" v-on:click="selectSchema(i)">{{i.schemaName}}</b-dropdown-item>
 
       </b-dropdown>
     <b-form-input></b-form-input>
       </b-input-group>
     </div>
-       
-
+       Hello!
+  selected:{{selectedSearchSchema}}
   </div>
 </template>
 
@@ -21,13 +21,22 @@ export default {
   name: "Search",
   data() {
     return {
+    searchLabel: 'Select Schema',
     selectedSearchSchema: '',
-    availableSearchSchema:[]
+    availableSearchSchema:[],
+
     }
   },
-
+ 
   created: function () {
         axios.get('/metalnx/api/search/indexes').then(response => (this.availableSearchSchema = response.data.searchSchemaEntry))
+  }, 
+  methods: {
+      selectSchema: function(selected) {
+        //console.log("eventData:" + eventData);
+        this.selectedSearchSchema = selected;
+        this.searchLabel = selected.schemaName;
+    },
   }
 };
 
