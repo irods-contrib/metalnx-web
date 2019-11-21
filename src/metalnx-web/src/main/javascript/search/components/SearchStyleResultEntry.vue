81e61305057f51bb62412c6fedc44c240c88cc83
deep.patel <deep.patel@nih.gov>
2019-11-21 13:52:18 -0500

    #113 search result fixes

--- src/metalnx-web/src/main/javascript/search/components/SearchStyleResultEntry.vue
+++ src/metalnx-web/src/main/javascript/search/components/SearchStyleResultEntry.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container">
+  <div class="container mb-3">
      <div class="row">
        <h3><b-link v-bind:href="searchResultEntry.url_link" target="_blank">{{ searchResultEntry.title }}</b-link></h3>
       </div>
       <div class="row">
         <p><span v-html="searchResultEntry.content_text"></span></p> 
       </div>
-    <b-button variant="link" v-on:click="isActive = !isActive" v-bind:id="searchResultEntry.title" v-bind:title="searchResultEntry.links.linkset_description">{{ searchResultEntry.links.linkset_title }} </b-button>
-    <div v-if="isActive">
-      <div v-for="item in searchResultEntry.links.links" v-bind:key="item.link_url">
-          <h8><b-link v-bind:href="item.link_url" target="_blank">{{ item.link_text }}</b-link></h8>
+      <div v-if="searchResultEntry.links.links.length > 0">
+         <b-button variant="link" size="lg" v-on:click="isActive = !isActive" v-bind:id="searchResultEntry.title" v-bind:title="searchResultEntry.links.linkset_description">{{ searchResultEntry.links.linkset_title }} </b-button>
+          <div v-if="isActive">
+            <div v-for="item in searchResultEntry.links.links" v-bind:key="item.link_url">
+              <h8><b-link v-bind:href="item.link_url" target="_blank">{{ item.link_text }}</b-link></h8>
+            </div>
         </div>
       </div>
   </div>
