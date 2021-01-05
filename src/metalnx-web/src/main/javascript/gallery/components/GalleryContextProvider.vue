e615c1854851e8cf046162c4238bef1c89607e52
connectbo <peterbozhou@gmail.com>
2021-01-05 09:27:27 -0500

    [#175] Create GalleryContentProvider

---
+++ src/metalnx-web/src/main/javascript/gallery/components/GalleryContextProvider.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- Search -->
+  <div class="container">
+   
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "GalleryContextProvider",
+  components: {
+  },
+  data() {
+    return {}
+  },
+}
+</script> 
+<style>
+</style>
