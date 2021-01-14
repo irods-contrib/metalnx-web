119bf34ae5fda71fd5c13a695982b3ff660ce360
connectbo <peterbozhou@gmail.com>
2021-01-14 14:15:12 -0500

    [#175] Update UI

--- src/metalnx-web/src/main/javascript/gallery/components/Gallery.vue
+++ src/metalnx-web/src/main/javascript/gallery/components/Gallery.vue
@@ -1,21 +1,198 @@
 <template>
   <!-- Search -->
-  <div class="container">
-   Hi from the gallery
+  <div>
+  <div class="widgetsContainer">
+    <span>{{formattedPath}}</span>
+    <span class="widgets">
+    <b-form-select v-model="selected" style=" width: 140px;" :options="options">
+    </b-form-select>
+    <b-link href="#" class="btn btn-default icon-btn"
+				id="showCollectionFormBtn" aria-label="add collection"
+				title="add collection"> <i
+				class="fa fa-folder"></i>
+			</b-link>
+    <b-link class="btn btn-default icon-btn"
+				:href="`/metalnx/collectionInfo${url}`"
+				id="infoIcon"
+				title="View info"><i class="fa fa-info-circle fa-color"></i>
+			</b-link>
+      <b-link href="#" class="btn btn-default icon-btn" aria-label="upload files" id="uploadIcon"
+				title="Upload file(s) to iRods"> <i
+				class="fa fa-cloud-upload fa-color"></i>
+			</b-link>
+    <b-link :href="`/metalnx/collections${url}`"
+    class="btn btn-default icon-btn"
+    id="collectionIcon"
+    title="Collection List View"><i class="fa fa-list fa-lg" aria-hidden="true"></i></b-link>
+    </span>
+  </div>
+    <div class="galleryContainer">
+      <div class="gallery" v-for="item in demo.items">
+        <a :href="`/metalnx/collectionInfo${url}%2F${item.name}`">
+        <img v-bind:class="[selected]" :src="item.thumbnails" />
+        <div class="thumbnail_name">{{ item.name }}</div>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "Gallery",
-  components: {
-  },
+  components: {},
   data() {
-    return {}
+    let url = window.location.search;
+    let params = new URLSearchParams(url.substring(1));
+    let path = decodeURIComponent(url.substring(9));
+    let formattedPath = path.replaceAll("/", " >> ")
+
+    let response = axios({
+      method: 'GET',
+      url: 'http://localhost/metalnx/api/gallery',
+      params:{
+        path: '/tempZone/home/test1',
+        offset: 0,
+        limit: 100
+      }
+    }).catch(e => console.log(e));
+
+    var demo = {
+      location: "testLocation",
+      items: [
+        {
+          id: 1,
+          name: "sample1.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/3873163/pexels-photo-3873163.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 2,
+          name: "sample2.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/4021808/pexels-photo-4021808.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 3,
+          name: "sample3.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/3825541/pexels-photo-3825541.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 4,
+          name: "sample4.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/3845729/pexels-photo-3845729.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 5,
+          name: "sample5.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/3683036/pexels-photo-3683036.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 6,
+          name: "sample6.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/4046948/pexels-photo-4046948.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 7,
+          name: "sample7.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/1250655/pexels-photo-1250655.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 8,
+          name: "sample8.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/3952224/pexels-photo-3952224.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 7,
+          name: "sample9.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/4033148/pexels-photo-4033148.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 8,
+          name: "sample10.png",
+          lastModified: "2020-12-14",
+          thumbnails: "https://images.pexels.com/photos/339619/pexels-photo-339619.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+      ],
+    };
+    return {
+      demo,
+      url,
+      formattedPath,
+      response,
+      selected: "medium",
+      options: ["small", "medium", "large"]
+    };
   },
-}
+};
 </script> 
 <style>
+.large {
+	width: 125px;
+	height: 125px;
+  }
+  
+  .galleryContainer {
+	max-width: 100%;
+	margin-left: 20px;
+	margin-top: 40px;
+	margin-right: 20px;
+	padding-left: 15px;
+	padding-right: 15px;
+	display: flex;
+	flex-flow: row wrap;
+	align-content: flex-start;
+  }
+  
+  
+  .medium {
+	width: 100px;
+	height: 100px;
+  }
+  
+  .small{
+	width: 75px;
+	height: 75px;
+  }
+  
+  .thumbnail_select{
+	width: 100px;
+	margin: 20px 10px auto 40px;
+  }
+  
+  .gallery{
+	margin: 10px 20px 10px 20px;
+	text-align: center;
+  }
+  
+  .thumbnail_name{
+	margin:5px auto 5px auto;
+  }
+
+  .collection_view{
+    float: right;
+    margin-top: 25px;
+    margin-right: 140px;
+  }
+
+  .widgetsContainer{
+    margin-top: 20px;
+    margin-left: 20px;
+    display:'flex';
+    flex-direction: column;
+  }
+
+  .widgets{
+    margin-left: 35%;
+  }
+
 </style>
