50fae99e73ab58a00f8937050b5a416a7fea9efa
connectbo <peterbozhou@gmail.com>
2021-01-20 16:22:05 -0500

    [#175] Pagination & PerPage

--- src/metalnx-web/src/main/javascript/gallery/components/Gallery.vue
+++ src/metalnx-web/src/main/javascript/gallery/components/Gallery.vue
@@ -1,38 +1,77 @@
 <template>
   <!-- Search -->
   <div>
-  <div class="widgetsContainer">
-    <span>{{formattedPath}}</span>
-    <span class="widgets">
-    <b-form-select v-model="selected" style=" width: 140px;" :options="options">
+    <div class="widgetsContainer">
+      <span>{{ formattedPath }}</span>
+      <span class="widgets">
+        <b-link
+          href="#"
+          class="btn btn-default icon-btn"
+          id="showCollectionFormBtn"
+          aria-label="add collection"
+          title="add collection"
+        >
+          <i class="fa fa-folder"></i>
+        </b-link>
+        <b-link
+          class="btn btn-default icon-btn"
+          :href="`/metalnx/collectionInfo${url}`"
+          id="infoIcon"
+          title="View info"
+          ><i class="fa fa-info-circle fa-color"></i>
+        </b-link>
+        <b-link
+          href="#"
+          class="btn btn-default icon-btn"
+          aria-label="upload files"
+          id="uploadIcon"
+          title="Upload file(s) to iRods"
+        >
+          <i class="fa fa-cloud-upload fa-color"></i>
+        </b-link>
+        <b-link
+          :href="`/metalnx/collections${url}`"
+          class="btn btn-default icon-btn"
+          id="collectionIcon"
+          title="Collection List View"
+          ><i class="fa fa-list fa-lg" aria-hidden="true"></i
+        ></b-link>
+      </span>
+    </div>
+    <div class="paginationContainer">
+    <b-pagination-nav 
+      active="true"
+      class="gallery-nav"
+      v-model="currentPage"
+      :link-gen="linkGen"
+      :number-of-pages="10"
+    />
+    <b-form-select
+          v-model="size_selected"
+          class="thumbnail-size-select"
+          :options="size_options"
+        >
+        <b-form-select-option :value="null" disabled>-- Select thumbnail size --</b-form-select-option>
     </b-form-select>
-    <b-link href="#" class="btn btn-default icon-btn"
-				id="showCollectionFormBtn" aria-label="add collection"
-				title="add collection"> <i
-				class="fa fa-folder"></i>
-			</b-link>
-    <b-link class="btn btn-default icon-btn"
-				:href="`/metalnx/collectionInfo${url}`"
-				id="infoIcon"
-				title="View info"><i class="fa fa-info-circle fa-color"></i>
-			</b-link>
-      <b-link href="#" class="btn btn-default icon-btn" aria-label="upload files" id="uploadIcon"
-				title="Upload file(s) to iRods"> <i
-				class="fa fa-cloud-upload fa-color"></i>
-			</b-link>
-    <b-link :href="`/metalnx/collections${url}`"
-    class="btn btn-default icon-btn"
-    id="collectionIcon"
-    title="Collection List View"><i class="fa fa-list fa-lg" aria-hidden="true"></i></b-link>
-    </span>
-  </div>
+    <b-form-select
+          v-model="items_selected"
+          class="thumbnail-items-select"
+          :options="items_options"
+          @change="updateItemPerPage(items_selected)"
+        >
+        <b-form-select-option :value="null" disabled>-- Select items per page --</b-form-select-option>
+    </b-form-select>
+    </div>
     <div class="galleryContainer">
       <div class="gallery" v-for="item in demo.items">
-        <a :id="'thumbnail'+item.id" :href="`/metalnx/collectionInfo${url}%2F${item.name}`">
-        <img v-bind:class="[selected]" :src="item.thumbnails" />
-        <div class="thumbnail_name">{{ item.name }}</div>
+        <a
+          :id="'thumbnail' + item.id"
+          :href="`/metalnx/collectionInfo${url}%2F${item.name}`"
+        >
+          <img v-bind:class="[size_selected]" :src="item.thumbnails" />
+          <div class="thumbnail_name">{{ item.name }}</div>
         </a>
-        <b-tooltip :target="'thumbnail'+item.id" triggers="hover">
+        <b-tooltip :target="'thumbnail' + item.id" triggers="hover">
           <div>File Size: {{ item.fileSize }}</div>
           <div>File Type: {{ item.fileType }}</div>
         </b-tooltip>
@@ -48,20 +87,25 @@ export default {
   name: "Gallery",
   components: {},
   data() {
-    let url = window.location.search;
-    let params = new URLSearchParams(url.substring(1));
-    let path = decodeURIComponent(url.substring(9));
-    let formattedPath = path.replaceAll("/", " >> ")
+    const currentUrl = window.location;
+    const url = window.location.search;
+    const params = new URLSearchParams(url.substring(1));
+    const path = decodeURIComponent(params.get("path"));
+    const formattedPath = path.substring(1).replaceAll("/", " >> ");
+
+    // Retrieve items per page and current page from url
+    const perPage = params.get("perPage");
+    const currentPage = params.get("currentPage");
 
     let response = axios({
-      method: 'GET',
-      url: 'http://localhost/metalnx/api/gallery',
-      params:{
-        path: '/tempZone/home/test1',
+      method: "GET",
+      url: "http://localhost/metalnx/api/gallery",
+      params: {
+        path: "/tempZone/home/test1",
         offset: 0,
-        limit: 100
-      }
-    }).catch(e => console.log(e));
+        limit: 100,
+      },
+    }).catch((e) => console.log(e));
 
     var demo = {
       location: "testLocation",
@@ -72,7 +116,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample1.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 2,
@@ -80,7 +124,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample2.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 3,
@@ -88,7 +132,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample3.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 4,
@@ -96,7 +140,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample7.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 5,
@@ -104,7 +148,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample5.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 6,
@@ -112,7 +156,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample6.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 7,
@@ -120,7 +164,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample7.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 8,
@@ -128,7 +172,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample8.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 9,
@@ -136,7 +180,7 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample9.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
         {
           id: 10,
@@ -144,79 +188,111 @@ export default {
           lastModified: "2020-12-14",
           thumbnails: "./img/sample10.png",
           fileSize: "10 MB",
-          fileType: "Portable Network Graphics"
+          fileType: "Portable Network Graphics",
         },
       ],
     };
     return {
       demo,
+      currentUrl,
       url,
+      path,
       formattedPath,
+      currentPage,
+      perPage,
       response,
-      selected: "medium",
-      options: ["small", "medium", "large"]
+      size_selected: null,
+      size_options: ["small", "medium", "large"],
+      items_selected: null,
+      items_options: [25,50,75,100],
     };
   },
+  methods: {
+    linkGen(pageNum) {
+      return `/gallery?path=${encodeURIComponent(this.path)}&currentPage=${pageNum}&perPage=${this.perPage}`
+    },
+    updateItemPerPage(newPerPage){
+      window.location = `/gallery?path=${encodeURIComponent(this.path)}&currentPage=${this.currentPage}&perPage=${newPerPage}`;
+    }
+  }
 };
 </script> 
 <style>
 .large {
-	width: 125px;
-	height: 125px;
-  }
-  
-  .galleryContainer {
-	max-width: 100%;
-	margin-left: 20px;
-	margin-top: 40px;
-	margin-right: 20px;
-	padding-left: 15px;
-	padding-right: 15px;
-	display: flex;
-	flex-flow: row wrap;
-	align-content: flex-start;
-  }
-  
-  
-  .medium {
-	width: 100px;
-	height: 100px;
-  }
-  
-  .small{
-	width: 75px;
-	height: 75px;
-  }
-  
-  .thumbnail_select{
-	width: 100px;
-	margin: 20px 10px auto 40px;
-  }
-  
-  .gallery{
-	margin: 10px 20px 10px 20px;
-	text-align: center;
-  }
-  
-  .thumbnail_name{
-	margin:5px auto 5px auto;
-  }
+  width: 125px;
+  height: 125px;
+}
 
-  .collection_view{
-    float: right;
-    margin-top: 25px;
-    margin-right: 140px;
-  }
+.galleryContainer {
+  max-width: 100%;
+  margin-left: 20px;
+  margin-top: 40px;
+  margin-right: 20px;
+  padding-left: 15px;
+  padding-right: 15px;
+  display: flex;
+  flex-flow: row wrap;
+  align-content: flex-start;
+}
 
-  .widgetsContainer{
-    margin-top: 20px;
-    margin-left: 20px;
-    display:'flex';
-    flex-direction: column;
-  }
+.medium {
+  width: 100px;
+  height: 100px;
+}
 
-  .widgets{
-    margin-left: 35%;
-  }
+.small {
+  width: 75px;
+  height: 75px;
+}
 
+.thumbnail_select {
+  width: 100px;
+  margin: 20px 10px auto 40px;
+}
+
+.gallery {
+  margin: 10px 20px 10px 20px;
+  text-align: center;
+}
+
+.gallery-nav{
+  margin: 10px 10px 10px 30px;
+}
+
+.thumbnail_name {
+  margin: 5px auto 5px auto;
+}
+
+.collection_view {
+  float: right;
+  margin-top: 25px;
+  margin-right: 140px;
+}
+
+.paginationContainer{
+  display: flex;
+  flex-direction: row;
+}
+
+.widgetsContainer {
+  margin-top: 20px;
+  margin-left: 20px;
+  display: "flex";
+  flex-direction: column;
+}
+.thumbnail-size-select{
+  margin-top: 10px;
+  margin-left: 10px;
+  width: auto;
+}
+
+.thumbnail-items-select{
+  margin-top: 10px;
+  margin-left: 10px;
+  width: auto;
+}
+
+.widgets {
+  margin-left: 35%;
+}
 </style>
