4f413cb0876022514f0de4378e40dce92010b8cc
connectbo <zbo@renci.org>
2021-04-29 18:40:10 -0400

    [#175] Update Gallery UI Components

---
+++ src/metalnx-web/src/main/javascript/gallery/components/GalleryItem.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="gallery-item">
+    <a
+      :id="'thumbnail' + item.id"
+      v-if="item.collection"
+      class="gallery-item_anchor"
+      :href="`/metalnx/gallery?path=${path}%2F${item.name}`"
+    >
+      <div class="gallery-thumbnail"><i class="fa fa-folder fa-3x" /></div>
+      <div class="gallery-item_name">{{ item.name }}</div>
+    </a>
+    <a
+      :id="'thumbnail' + item.id"
+      v-else
+      class="gallery-item_anchor"
+      :href="`/metalnx/collectionInfo?path=${path}%2F${item.name}`"
+    >
+      <div class="gallery-thumbnail">
+        <img
+          alt="Thumbnail Image"
+          :src="item.previewSrc"
+          class="gallery-thumbnail"
+        />
+      </div>
+      <div class="gallery-item_name">{{ item.name }}</div>
+    </a>
+    <b-tooltip :target="'thumbnail' + item.id" triggers="hover">
+      <div>Name: {{ item.name }}</div>
+      <div>{{ item.hover }}</div>
+    </b-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GalleryItem",
+  components: {},
+  props: ["item", "path"],
+  data() {
+    return {};
+  },
+};
+</script> 
+<style>
+.gallery-item {
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+  height: 200px;
+  margin: 10px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.gallery-item_name {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 150px;
+}
+
+.gallery-item_anchor:hover {
+  text-decoration: none;
+}
+
+.gallery-thumbnail {
+  height: 150px;
+  width: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  object-fit: scale-down;
+}
+</style>
