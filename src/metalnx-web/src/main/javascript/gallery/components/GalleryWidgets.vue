16ba48130fb46f8e62931ba1014b262145d519fb
connectbo <zbo@renci.org>
2021-04-29 18:40:10 -0400

    [175] Update Gallery UI Components
    create galleryitem
    
    render static file icon
    
    implement and style navigation
    
    refactor UI and error handling

---
+++ src/metalnx-web/src/main/javascript/gallery/components/GalleryWidgets.vue
@@ -0,0 +1,49 @@
+<template>
+  <span>
+    <b-link
+      href="#"
+      class="btn btn-default icon-btn"
+      id="showCollectionFormBtn"
+      aria-label="add collection"
+      title="add collection"
+    >
+      <i class="fa fa-folder"></i>
+    </b-link>
+    <b-link
+      class="btn btn-default icon-btn"
+      :href="`/metalnx/collectionInfo${url}`"
+      id="infoIcon"
+      title="View info"
+      ><i class="fa fa-info-circle fa-color"></i>
+    </b-link>
+    <b-link
+      href="#"
+      class="btn btn-default icon-btn"
+      aria-label="upload files"
+      id="uploadIcon"
+      title="Upload file(s) to iRods"
+    >
+      <i class="fa fa-cloud-upload fa-color"></i>
+    </b-link>
+    <b-link
+      :href="`/metalnx/collections${url}`"
+      class="btn btn-default icon-btn"
+      id="collectionIcon"
+      title="Collection List View"
+      ><i class="fa fa-list fa-lg" aria-hidden="true"></i
+    ></b-link>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "GalleryWidgets",
+  components: {},
+  props: ['url'],
+  data() {
+    return {};
+  },
+};
+</script> 
+<style>
+</style>
