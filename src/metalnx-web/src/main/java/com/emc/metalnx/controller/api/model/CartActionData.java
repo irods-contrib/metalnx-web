b737fad5b2ac93275f4510c2d90f7e4989dff207
Mike Conway <mike.conway@nih.gov>
2021-05-27 10:40:21 -0400

    #3 remove indiv items from cart

---
+++ src/metalnx-web/src/main/java/com/emc/metalnx/controller/api/model/CartActionData.java
@@ -0,0 +1,14 @@
+package com.emc.metalnx.controller.api.model;
+
+public class CartActionData {
+	private String[] paths;
+
+	public String[] getPaths() {
+		return paths;
+	}
+
+	public void setPaths(String[] paths) {
+		this.paths = paths;
+	}
+
+}
