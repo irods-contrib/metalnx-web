7194ae31878a8d41c66c8a3c1f67d073fd456aa3
Mike Conway <Michael.c.conway@gmail.com>
2022-02-18 15:50:30 -0500

    #4 fix metadata search results

--- src/metalnx-web/src/main/javascript/search/components/SearchStyleResultList.vue
+++ src/metalnx-web/src/main/javascript/search/components/SearchStyleResultList.vue
@@ -10,7 +10,7 @@
          <b-button variant="link" size="lg" v-on:click="isActive = !isActive" v-bind:id="searchResultEntry.title" v-bind:title="searchResultEntry.links.linkset_description">{{ searchResultEntry.links.linkset_title }} </b-button>
           <div v-if="isActive">
             <div v-for="item in searchResultEntry.links.links" v-bind:key="item.link_url">
-              <h8><b-link v-bind:href="item.link_url" target="_blank">{{ item.link_text }}</b-link></h8>
+              <h8><b-link v-bind:href="item.metalnx_relative_url" target="_blank">{{ item.link_text }}</b-link></h8>
             </div>
         </div>
       </div>
